dabe47e82a43a61e170328e0e8bb27515e6b5044
qssha <qssha@yandex.ru>
2024-01-18 17:41:27 +0300

    Irradiation script initialization

---
+++ stellar_wind_irradiation/adjust_mean_intensity_for_irrad.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+
+SPEED_OF_LIGHT_CGS = 2.99702547 * 10**10
+
+def get_lines_from_file_by_number(file_to_process, line_numbers):
+    with open(file_to_process) as current_file:
+        return [line.strip() for i, line in enumerate(current_file) if i in line_numbers]
+
+def parse_meanopac(file_dir, ND):
+    file_path = file_dir + "/MEANOPAC"
+    meanopac_file_lines = get_lines_from_file_by_number(file_path, list(range(ND + 1)))
+    data_frame_header = meanopac_file_lines[0].split()
+    meanopac_data = [entry.split() for entry in meanopac_file_lines[1:]] 
+    meanopac_data_frame = pd.DataFrame(meanopac_data, columns=data_frame_header)
+    return meanopac_data_frame.astype("float64")
+
+def parse_eddfactor_file(file_dir, ND):
+    file_path = file_dir + "/EDDFACTOR"
+    dtype_string = "(" + str(ND + 1) + ",)float64"
+    eddfactor_array = np.fromfile(file_path, dtype=dtype_string)
+    return eddfactor_array
+
+def x_ray_mean_intensity(x_ray_lum, distance, delta_tau):
+    return (x_ray_lum / (16 * np.pi**2 * distance**2)) * np.exp(-delta_tau)
+
+def get_uniform_x_ray_lum(full_x_ray_lum):
+    return full_x_ray_lum / (SPEED_OF_LIGHT_CGS * 0.999 / (10**-8))
+
+def modify_eddfactor(file_dir, ND, full_x_ray_lum, x_ray_source_orbit_radius, low_freq_limit=3.2862121381578944 * 10**15, high_freq_limit=3.7462818375 * 10**15):
+    meanopac_data_frame = parse_meanopac(file_dir, ND)
+    meanopac_data_frame['R'] = meanopac_data_frame['R'] * 10**10
+    tau_es_interp = interpolate.interp1d(meanopac_data_frame['R'], meanopac_data_frame['Tau(es)'])
+
+    delta_distance_array = np.abs(x_ray_source_orbit_radius - meanopac_data_frame['R'])
+    delta_tau_es_array = np.abs(tau_es_interp(x_ray_source_orbit_radius) - meanopac_data_frame['Tau(es)'])
+
+    eddfactor_array = parse_eddfactor_file(file_dir, ND)
+
+    # Uniform law
+    mean_intensity_for_uniform_lum = lambda delta_distance, delta_tau: x_ray_mean_intensity(get_uniform_x_ray_lum(full_x_ray_lum), delta_distance, delta_tau)
+    print(mean_intensity_for_uniform_lum(delta_distance_array, delta_tau_es_array))
+    eddfactor_array[(eddfactor_array[:, -1] > low_freq_limit / 10**15) & (eddfactor_array[:, -1] < high_freq_limit / 10**15), :-1] += mean_intensity_for_uniform_lum(delta_distance_array, delta_tau_es_array) 
+    #
+
+    #return mean_intensity_for_uniform_lum(delta_distance_array, delta_tau_es_array) 
+    return eddfactor_array
+
+file_dir = "."
+ND = 55
+
+eddfactor_array_mod = modify_eddfactor(file_dir, ND, 5 * 10**40, 9.4 * 10**12)
+eddfactor_array_mod.tofile("EDDFACTOR_MOD")
