d9d0de6dab5757774b8231e6a2d788f998f9e57c
qssha <qssha@yandex.ru>
2024-01-18 17:09:43 +0300

    Latest updates to wind beta law script

--- wind_beta_law_plot/speed.py
+++ wind_beta_law_plot/speed.py
@@ -10,23 +10,26 @@ def double_beta_law(Vcore, V0, Vinf1, Vinf2, beta1, beta2, Scl_ht, Rstar, r):
     return (V0 + (Vinf - Vext - V0) * (1 - Rstar / r)**beta1 + Vext * (1 - Rstar / r)**beta2) / (1 + (V0 / Vcore) * np.exp((Rstar - r) / Scl_ht))
 
 def main():
-    Vcore = 0.8
+    Vcore = 0.07
     V0 = 30
-    Vinf = 250
-    beta = 30.0
-    Rstar = 139.2
-    Scl_ht = 0.022
-    rmax = 1000 * Rstar
-    R = np.linspace(Rstar, rmax, num = 1000)
-    velocity = np.array([simple_beta_law(Vcore, V0, Vinf, beta, Scl_ht, Rstar * 10**5, r * 10**5) for r in R])
-    #velocity2 = np.array([simple_beta_law(Vcore, V0, Vinf, beta, Scl_ht, Rstar * 10**5, r * 10**5) for r in  R])
+    Vinf = 185
+    beta = 2.5
+    Rstar = 528.9 
+    Scl_ht = 0.022 * Rstar
+    rmax = 200 * Rstar
+    R = np.geomspace(Rstar, rmax, num = 1000)
+    velocity = np.array([simple_beta_law(Vcore, V0, Vinf, beta, Scl_ht, Rstar, r) for r in R])
+    velocity2 = np.array([simple_beta_law(Vcore, 15, Vinf, 2.0, 0.015 * Rstar, Rstar, r) for r in  R])
 
-    velocity3 = np.array(map(lambda x: double_beta_law(Vcore, 100, 150, 450, beta, 700.0, Scl_ht, Rstar * 10**5, x * 10**5), R))
+    #velocity = np.array(map(lambda x: double_beta_law(Vcore, 50, 80, 250, 4, 50.0, Scl_ht, Rstar, x), R))
+    #velocity2 = np.array([simple_beta_law(Vcore, 10, 200, 15, Scl_ht, Rstar, r) for r in R])
+    #velocity3 = np.array([simple_beta_law(Vcore, 10, 200, 20, Scl_ht, Rstar, r) for r in R])
+    #velocity3 = np.array(map(lambda x: double_beta_law(Vcore, 30, 300, 1000, beta, 300.0, Scl_ht, Rstar, x), R))
 
     plt.figure()
-    plt.plot(R / Rstar, velocity, 'r')
-    #plt.plot(np.log10(R / Rstar), velocity2, 'b')
-    plt.plot(R / Rstar, velocity3, 'g')
+    plt.plot(np.log10(R / Rstar), velocity, 'r')
+    plt.plot(np.log10(R / Rstar), velocity2, 'b')
+    #plt.plot(np.log10(R / Rstar), velocity3, 'g')
 
     #plt.xlim(Rstar - 5, rmax + 10)
     #plt.ylim(0, 400)
